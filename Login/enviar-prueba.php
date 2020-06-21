826bbfa9a02d16fea8f491c1fa3232cc017b342c
JoseCampos17 <66966452+JoseCampos17@users.noreply.github.com>
2020-06-21 12:44:40 -0500

    Update enviar-prueba.php

--- Login/enviar-prueba.php
+++ Login/enviar-prueba.php
@@ -14,7 +14,7 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'jose12345uni@gmail.com';                     // SMTP username
+    $mail->Username   = 'emiliosantiago134s5@gmail.com';                     // SMTP username
     $mail->Password   = '6652278Jogf';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
@@ -22,7 +22,7 @@ try {
     //Recipients
     $sql = 'SELECT * FROM reserva';
         foreach ($conn->query($sql) as $row) {
-            $mail->setFrom('jose12345uni@gmail.com', 'ReservasCanchas.com');
+            $mail->setFrom('emiliosantiago134s5@gmail.com', 'ReservasCanchas.com');
             $mail->addAddress( $row['correo'] ); 
              
                                 
